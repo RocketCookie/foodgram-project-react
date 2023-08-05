4806ea71795056b59c11f9338a962c9c72dc3bed
Олег Гуров <guroff.oleg@gmail.com>
2023-08-05 17:57:37 +0300

    Add docstring

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,11 +2,18 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class AuthorOrReadOnly(BasePermission):
+    '''
+    Правило доступа, позволяющее только автору объекта изменять его,
+    в то время как остальным разрешены только безопасные методы
+    (GET, HEAD, OPTIONS).
+    '''
+
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         return obj.author == request.user
 
+# взял из исходников IsAuthenticatedOrReadOnly
     def has_permission(self, request, view):
         return bool(
             request.method in SAFE_METHODS
