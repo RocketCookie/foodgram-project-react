c3adb25926f8ef04c0215ebbdd1a09275fa8d025
Олег Гуров <guroff.oleg@gmail.com>
2023-08-08 22:28:33 +0300

    Fix recipe filter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,40 +1,45 @@
-from django_filters import BooleanFilter, FilterSet, ModelMultipleChoiceFilter
+from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
 
 from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
-    '''Фильтр для модели Recipe.'''
+    '''
+    Фильтр для модели Recipe.
+    '''
 
-    tags = ModelMultipleChoiceFilter(
+    tags = filters.ModelMultipleChoiceFilter(
         queryset=Tag.objects.all(),
         field_name='tags__slug',
         to_field_name='slug',
     )
-    is_favorite = BooleanFilter(method='filter_in_favorite')
-    is_in_shopping_card = BooleanFilter(method='filter_is_in_shopping_card')
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorite', 'is_in_shopping_card')
+        fields = ('tags', 'author')
 
-    def filter_in_favorite(self, queryset, name, value):
-        '''Фильтрует рецепты по наличию в избранном пользователя.'''
+    def filter_is_favorited(self, queryset, name, value):
+        '''
+        Фильтрует рецепты по наличию в избранном пользователя.
+        '''
         user = self.request.user
         if value and user.is_authenticated:
             return queryset.filter(is_favorited__user=user)
         return queryset
 
-    def filter_is_in_shopping_card(self, queryset, name, value):
-        '''Фильтрует рецепты по наличию в корзине покупок пользователя.'''
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        '''
+        Фильтрует рецепты по наличию в корзине покупок пользователя.
+        '''
         user = self.request.user
         if value and user.is_authenticated:
-            return queryset.filter(is_in_shopping_card__recipe=user)
+            return queryset.filter(is_in_shopping_cart__user=user)
         return queryset
-
-
-# /api/recipes/?page=1&limit=6&is_favorited=1&author=2&tags=red&tags=test
 
 
 class CustomSearchFilter(SearchFilter):
